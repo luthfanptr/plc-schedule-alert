5c31b5689ce2b1012d3d4bafaaf1397ffc5d3df9
Luthfan Putra Prakarsa <luthfanputra.04@gmail.com>
2026-06-08 16:45:41 +0700

    feat: add excel service provider and job scheduler

--- src/bootstrap/providers.php
+++ src/bootstrap/providers.php
@@ -1,7 +1,10 @@
 <?php
 
+declare(strict_types=1);
+
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\Filament\ControlPanelProvider::class,
+    Maatwebsite\Excel\ExcelServiceProvider::class,
 ];
